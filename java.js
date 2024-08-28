82b4d0e56dcbee4791f4427fc3452e91a3c8a651
Laura Hove Stuckert <last0003@stud.kea.dk>
2024-08-28 20:39:15 +0200

    øvelse 2 udført

--- java.js
+++ java.js
@@ -20,3 +20,41 @@ Betingelse if-else:
 - bestemmer hvilken tekst der udskrives i consolen baseret på værdien af alt-attributen 
 - console.log => hvilken tekst der skal udskrives 
 */
+
+/* ØVELSE 2 */
+
+/* Oprette funktion og give den et navn */
+function siggoddag() {
+  /*Få det aktuelle timetal
+   - new date => et nyt Date objekt som indeholder alle oplysninger om den aktuelle dato, tid inkl. år, måned, dag osv. 
+   - getHours => en metode der henter den aktuelle time fra date-objektet som et tal ml. 0-23
+       - hours er en konstant variabel, så denne værdi kan bruges i resten af funktionen  */
+
+  const hour = new Date().getHours();
+
+  // Logik til at bestemme hilsenen baseret på timetallet
+  if (hour >= 5 && hour < 10) {
+    console.log("Godmorgen");
+  } else if (hour >= 10 && hour < 18) {
+    console.log("Goddag");
+  } else if (hour >= 18 && hour < 24) {
+    console.log("Godaften");
+  } else {
+    console.log("Godnat");
+  }
+}
+
+/* if else er en struktur man bruger til at udføre forskellige koder alt efter, hvilken betingelse der er sand. 
+
+Betingelse forklaret: 
+if ( hour >=5 && hour < 10)
+- værdien af hour er større eller lig med 5 OG mindre end 10 er denne betingelse sand og koden vil være gældende
+
+- else => den kode der bliver udskrevet hvis ingen af ovenstående betingelser er sande */
+
+/* console log => udskriver beskede til webbrowserens konsol */
+
+// Kald funktionen for at udskrive hilsenen i konsollen
+siggoddag();
+
+/* At kalde funktionen er nødvendig for at koden skal køre. Dette gøre med tom parentes */
